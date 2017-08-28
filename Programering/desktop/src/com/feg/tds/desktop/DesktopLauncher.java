316a17e416ed1f722a335b63819e397c45d633e0
weglon <31338623+weglon@users.noreply.github.com>
2017-08-28 13:44:02 +0200

    sätter igång
    Lägger till lite mappar samt tomt libgdx project

---
+++ Programering/desktop/src/com/feg/tds/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.feg.tds.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.feg.tds.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
